c69612fc9a0206adb324a3ecf8be090d60b37dfe
宮岡聖也 <miyaoka@diveintocode.jp>
2024-03-06 13:57:20 +0900

    fix: checkout

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -26,23 +26,12 @@ class Cart
 
   def check_out
     return if owner.wallet.balance < total_amount
-
-    self.items.each do |item|
-      item.owner.wallet.deposit(self.owner.wallet.withdraw(item.price))
+    @items.each do |item|
+      self.owner.wallet.withdraw(item.price)
+      item.owner.wallet.deposit(item.price) 
       item.owner = self.owner
     end
-
     @items = []
-  # ## 要件
-  #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
-  #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
-  #   - カートの中身（Cart#items）が空になること。
-
-  # ## ヒント
-  #   - カートのオーナーのウォレット ==> self.owner.wallet
-  #   - アイテムのオーナーのウォレット ==> item.owner.wallet
-  #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-  #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
   end
 
 end
